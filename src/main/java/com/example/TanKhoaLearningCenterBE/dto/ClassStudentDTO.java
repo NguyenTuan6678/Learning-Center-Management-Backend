bbc9593df1c6acc0cbf8e8916a07139bc87cc45d
HarryNguyen6678 <lou9vn@gmail.com>
2025-05-22 22:12:20 +0700

    ☾ BACKEND DONE ☽

--- src/main/java/com/example/TanKhoaLearningCenterBE/dto/ClassStudentDTO.java
+++ src/main/java/com/example/TanKhoaLearningCenterBE/dto/ClassStudentDTO.java
@@ -1,23 +1,66 @@
 package com.example.TanKhoaLearningCenterBE.dto;
 
+import com.example.TanKhoaLearningCenterBE.entity.ClassEntity;
 import com.example.TanKhoaLearningCenterBE.entity.ClassStudentEntity;
 import lombok.Data;
 
+import java.time.LocalTime;
 import java.util.UUID;
 
 @Data
 public class ClassStudentDTO {
     private UUID id;
-    private UUID classId;
-    private String className;
     private UUID studentId;
     private String studentName;
 
-    public ClassStudentDTO(ClassStudentEntity classStudentEntity) {
-        this.id = classStudentEntity.getClassStudentId();
-        this.classId = classStudentEntity.getClazz().getClassId();
-        this.className = classStudentEntity.getClazz().getClassName();
-        this.studentId = classStudentEntity.getStudent().getStudentId();
-        this.studentName = classStudentEntity.getStudent().getStdName();
+    private UUID classId;
+    private String className;
+    private String description;
+
+    private UUID courseId;
+    private String courseName;
+
+    private UUID teacherId;
+    private String teacherName;
+
+    private UUID dayId;
+    private String dayName;
+
+    private UUID timeId;
+    private LocalTime timeStart;
+    private LocalTime timeEnd;
+
+    public ClassStudentDTO(ClassStudentEntity entity) {
+        this.id = entity.getClassStudentId();
+        this.studentId = entity.getStudent().getStudentId();
+        this.studentName = entity.getStudent().getStdName();
+
+        ClassEntity clazz = entity.getClazz();
+        if (clazz != null) {
+            this.classId = clazz.getClassId();
+            this.className = clazz.getClassName();
+            this.description = clazz.getDescription();
+
+            if (clazz.getCourse() != null) {
+                this.courseId = clazz.getCourse().getCourseId();
+                this.courseName = clazz.getCourse().getCourseName();
+            }
+
+            if (clazz.getTeacher() != null) {
+                this.teacherId = clazz.getTeacher().getTeacherId();
+                this.teacherName = clazz.getTeacher().getTeacherName();
+            }
+
+            if (clazz.getDay() != null) {
+                this.dayId = clazz.getDay().getDayId();
+                this.dayName = clazz.getDay().getDay();
+            }
+
+            if (clazz.getTime() != null) {
+                this.timeId = clazz.getTime().getTimeId();
+                this.timeStart = clazz.getTime().getTimeStart();
+                this.timeEnd = clazz.getTime().getTimeEnd();
+            }
+        }
     }
 }
